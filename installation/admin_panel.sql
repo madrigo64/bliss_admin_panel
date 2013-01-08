1cb6e85dfb609e4b52c692f1031f67ebed3e52e8
SkynetDev <skynetdev3@gmail.com>
2013-01-08 23:57:08 +0200

    обновил дамп

--- installation/admin_panel.sql
+++ installation/admin_panel.sql
@@ -3,91 +3,15 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Дек 24 2012 г., 15:55
+-- Время создания: Янв 08 2013 г., 22:55
 -- Версия сервера: 5.5.27
 -- Версия PHP: 5.4.7
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
--- База данных: `dayz`
+-- База данных: `dayz_longhost`
 --
-
-DELIMITER $$
---
--- Процедуры
---
-DROP PROCEDURE IF EXISTS `proc_getBuildingPageCount`$$
-CREATE DEFINER=`dayz`@`localhost` PROCEDURE `proc_getBuildingPageCount`(in `p_instanceId` int)
-begin
-  declare itemsPerPage int default 5; -- must match proc_getobjects
-  select
-    floor(count(*) / itemsPerPage) + if((count(*) mod itemsPerPage) > 0, 1, 0)
-  from instance_building
-  where instance_id = p_instanceId; --
-end$$
-
-DROP PROCEDURE IF EXISTS `proc_getBuildings`$$
-CREATE DEFINER=`dayz`@`localhost` PROCEDURE `proc_getBuildings`(in `p_instanceId` int, in `p_currentPage` int)
-begin
-  set @instance = p_instanceId; --
-  set @page = greatest(((p_currentPage - 1) * 5), 0); --
-  prepare stmt from '
-  select
-    b.class_name, ib.worldspace
-  from
-    instance_building ib
-    inner join building b on ib.building_id = b.id
-  where
-    ib.instance_id = ?
-  limit ?, 5'; --
-  execute stmt using @instance, @page; --
-  deallocate prepare stmt; --
-end$$
-
-DROP PROCEDURE IF EXISTS `proc_getMessagePage`$$
-CREATE DEFINER=`dayz`@`localhost` PROCEDURE `proc_getMessagePage`(in `p_instanceId` int, in `p_currentPage` int)
-begin
-  set @instance = p_instanceId; --
-  set @page = greatest(((p_currentPage - 1) * 10), 0); --
-  prepare stmt from 'select payload, loop_interval, start_delay  from message where instance_id = ? limit ?, 10'; --
-  execute stmt using @instance, @page; -- 
-  deallocate prepare stmt; --
-end$$
-
-DROP PROCEDURE IF EXISTS `proc_getMessagePageCount`$$
-CREATE DEFINER=`dayz`@`localhost` PROCEDURE `proc_getMessagePageCount`(in `p_instanceId` int)
-begin
-  declare itemsPerPage int default 10; -- must match proc_getMessagePage
-  select
-    floor(count(*) / itemsPerPage) + if((count(*) mod itemsPerPage) > 0, 1, 0)
-  from message
-  where message.instance_id = p_instanceId; --
-end$$
-
-DROP PROCEDURE IF EXISTS `proc_loglogin`$$
-CREATE DEFINER=`dayz`@`localhost` PROCEDURE `proc_loglogin`(in `p_uniqueId` varchar(128), in `p_instanceId` int)
-begin
-  insert into log_entry (unique_id, instance_id, log_code_id) values (p_uniqueId, p_instanceId, 1); --
-end$$
-
-DROP PROCEDURE IF EXISTS `proc_loglogout`$$
-CREATE DEFINER=`dayz`@`localhost` PROCEDURE `proc_loglogout`(in `p_uniqueId` varchar(128), in `p_instanceId` int)
-begin
-  insert into log_entry (unique_id, instance_id, log_code_id) values (p_uniqueId, p_instanceId, 5); --
-end$$
-
-DELIMITER ;
-
--- --------------------------------------------------------
-
 --
 -- Структура таблицы `adm_admins`
 --
@@ -109,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `adm_admins` (
 --
 
 INSERT INTO `adm_admins` (`id`, `login`, `password`, `guid`, `lastlogin`, `permissions`) VALUES
-(1, 'superadmin', 'e10adc3949ba59abbe56e057f20f883e', '', '2012-12-24 00:48:56', 'control,admins,chat,entities,map,');
+(1, 'superadmin', 'e10adc3949ba59abbe56e057f20f883e', '', '2012-12-24 02:48:56', 'control,admins,chat,entities,map,');
 
 -- --------------------------------------------------------
 
@@ -621,7 +545,7 @@ INSERT INTO `adm_objects` (`id`, `class_name`, `name`, `type`, `subtype`, `trans
 (464, 'Binocular', 'sBinocular', 'item', 'binocular', 0, 0, 0, 0, 1, 'binocular', 0, 1),
 (465, 'Laserdesignator', 'sLaserdesignator', 'item', 'binocular', 0, 0, 0, 0, 1, 'binocular', 0, 0),
 (466, 'Laserdesignator_mounted', 'sLaserdesignator_mounted', 'item', 'binocular', 0, 0, 0, 0, 1, 'binocular', 0, 0),
-(467, 'Binocular_Vector', 'sBinocular_Vector', 'item', 'binocular', 0, 0, 0, 0, 1, 'binocular', 0, 0),
+(467, 'Binocular_Vector', 'sBinocular_Vector', 'item', 'binocular', 0, 0, 0, 0, 1, 'binocular', 0, 1),
 (468, 'bizon', 'sbizon', 'item', 'rifle', 0, 0, 0, 0, 10, 'rifle', 0, 0),
 (469, 'bizon_silenced', 'sbizon_silenced', 'item', 'rifle', 0, 0, 0, 0, 10, 'rifle', 0, 1),
 (470, 'BoltSteel', 'sBoltSteel', 'item', 'heavyammo', 0, 0, 0, 0, 1, 'heavyammo', 0, 1),
@@ -880,7 +804,3 @@ CREATE TABLE IF NOT EXISTS `adm_players_log` (
   `created_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
